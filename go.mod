646cbebbfeb526dd6ab72fbf0bfb5f8e2e547b13
calledA <iamwzxin@163.com>
2022-08-27 18:02:58 +0800

    cache

---
+++ go.mod
@@ -0,0 +1,7 @@
+module gmr/gmr-cache
+
+go 1.18
+
+require github.com/golang/protobuf v1.5.2
+
+require google.golang.org/protobuf v1.26.0 // indirect
---
+++ gove/peers.go
@@ -0,0 +1,14 @@
+package gove
+
+import pb "gmr/gmr-cache/gove/geecachepb"
+
+// PeerPicker is the interface that must be implemented to locate
+// the peer that owns a specific key.
+type PeerPicker interface {
+	PickPeer(key string) (peer PeerGetter, ok bool)
+}
+
+// PeerGetter is the interface that must be implemented by a peer.
+type PeerGetter interface {
+	Get(in *pb.Request, out *pb.Response) error
+}
